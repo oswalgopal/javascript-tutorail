0b94ec6a222e57027a2949fc960a7bd3ec64d718
oswalgopal <gploswal@gmail.com>
2019-02-12 21:46:52 +0530

    some more topics added

---
+++ 4. comments in js /comment.js
@@ -0,0 +1,12 @@
+//to create a one line comment we use "//" 
+// for double line comments we use /*  */
+
+
+
+//This is a single line comment
+/* This    
+    is
+    a 
+    multi line 
+    comment
+     */
---
+++ 5. alert function/alert_ function.js
@@ -0,0 +1,15 @@
+//this is a function method which forcefully give instruction to the user
+    //syntax window.alert("alert message");
+
+
+
+//example
+
+    window.alert("hello user");
+    window.alert("this is a alert message ");
+
+    
+    
+
+
+
