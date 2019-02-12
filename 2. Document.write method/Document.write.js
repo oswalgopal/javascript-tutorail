b06c9efa5f0361aef8dbe5f690754f32aa5b96e5
oswalgopal <gploswal@gmail.com>
2019-02-12 21:31:04 +0530

    some tutorial added

---
+++ 2. Document.write method/Document.write.js
@@ -0,0 +1,5 @@
+// Document.writee method help us to print the content on the screen 
+
+document.write("This is the use of document.write method to print content on screen ");
+document.write(7);   // output= 7
+document.write(2+6); //output=8
---
+++ 3. console.log/cosole.log.js
@@ -0,0 +1,9 @@
+//console.log is a method which help us to print the content in the console 
+//you can access the console inside the developers tools or by using this shortcut Ctrl+shift+I
+
+
+
+console.log("hello user this is console window");
+console.log(7);  //7
+console.log(6+2); // 8
+
